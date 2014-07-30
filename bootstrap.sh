a9b56981c1cb636f8ce9db010867549a118d38bd
Paweł J. Wal <pjw@paweljw.eu>
2014-07-30 18:33:11 +0200

    Locale pl_PL

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,19 +11,9 @@ function bang()
 	echo "# Instalacja GITa"
 	apt-get update
 	apt-get -qq -y --force-yes install git mc
-	
+
 	echo "# Ide do /tmp"
 	cd /tmp
-
-	dpkg-reconfigure locales
-	update-locale LANG="pl_PL.UTF-8" LANGUAGE="pl_PL"
-
-	export LANGUAGE=pl_PL.UTF-8
-	export LANG=pl_PL.UTF-8
-	export LC_ALL=pl_PL.UTF-8
-
-	locale-gen pl_PL
-	locale-gen pl_PL.UTF-8
 
 	echo "# Klonuje skrypty rozruchowe"
 	rm -rf debian-recovery
